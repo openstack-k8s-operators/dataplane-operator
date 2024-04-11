ba95d629a2cca3e6a5f621114b70eb869107cdd2
James Slagle <jslagle@redhat.com>
2024-04-11 17:46:09 -0400

    Add service and deployment webhooks for clean_local_webhook.sh
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- hack/clean_local_webhook.sh
+++ hack/clean_local_webhook.sh
@@ -2,4 +2,9 @@
 set -ex
 
 oc delete validatingwebhookconfiguration/vopenstackdataplanenodeset.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vopenstackdataplanedeployment.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vopenstackdataplaneservice.kb.io --ignore-not-found
+
 oc delete mutatingwebhookconfiguration/mopenstackdataplanenodeset.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mopenstackdataplaneservice.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/mopenstackdataplanedeployment.kb.io --ignore-not-found
